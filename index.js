afd0cfa5199088b9e62d98c2d028553cce607511
Bob <zncodebox@gmail.com>
2019-08-29 17:05:49 -0400

    a

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var http = require('http').Server(app);
+
+// -------------- serve static folders -------------- //
+
+app.use('/html', express.static(path.join(__dirname, 'html')))
+app.use('/js', express.static(path.join(__dirname, 'js')))
+app.use('/css', express.static(path.join(__dirname, 'css')))
+app.use('/data/uploaded_files', express.static(path.join(__dirname, '/data/uploaded_files')))
+app.use('/images/icon', express.static(path.join(__dirname, 'images/icon')))
+app.use(express.json())
+  
+// -------------- middleware functions -------------- //
+
+// -------------- bodyParser setup -------------- //
+
+app.use(bodyParser.urlencoded({ extended: false})); 
+app.use(bodyParser.json());
+
+
+app.get('/', function(req,res) {
+    res.sendFile(__dirname + "/html/home.html");
+})
+
+http.listen(process.env.PORT, process.env.IP);
