4ad20a67c522e783b059b10497d17e2364dff3cb
Bob <zncodebox@gmail.com>
2020-03-22 18:35:56 -0400

    s

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', function(req,res) {
     res.sendFile(__dirname + "/html/home.html");
 })
 
-app.get('/', function(req,res) {
+app.get('/test', function(req,res) {
     res.sendFile(__dirname + "/html/test.html");
 })
 
