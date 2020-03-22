c5fc678ecfe8f85d48d453777ef9e242cd1d31db
Bob <zncodebox@gmail.com>
2020-03-22 18:10:44 -0400

    s

--- js/home.js
+++ js/home.js
@@ -1,35 +1,35 @@
 // Init ScrollMagic
 var ctrl = new ScrollMagic.Controller({
-    globalSceneOptions: {
-      triggerHook: 'onLeave'
-    }
-  });
+  globalSceneOptions: {
+    triggerHook: 'onLeave'
+  }
+});
+
+// Create scene
+$("section").each(function() {
+
+  var name = $(this).attr('id');
   
-  // Create scene
-  $("section").each(function() {
-  
-    var name = $(this).attr('id');
-    
-    new ScrollMagic.Scene({
-      triggerElement: this
-    })
-    .setPin(this)
-    .addIndicators({
-      colorStart: "rgba(255,255,255,0.5)",
-      colorEnd: "rgba(255,255,255,0.5)", 
-      colorTrigger : "rgba(255,255,255,1)",
-      name:name
-      })
-    .loglevel(3)
-    .addTo(ctrl);
-   
-  });
-  
-  // Get window height
-  var wh = window.innerHeight;
-   
   new ScrollMagic.Scene({
-    offset: wh*3
+    triggerElement: this
   })
-  .setClassToggle("section#four", "is-active")
+  .setPin(this)
+  .addIndicators({
+    colorStart: "rgba(255,255,255,0.5)",
+    colorEnd: "rgba(255,255,255,0.5)", 
+    colorTrigger : "rgba(255,255,255,1)",
+    name:name
+	})
+  .loglevel(3)
   .addTo(ctrl);
+ 
+});
+
+// Get window height
+var wh = window.innerHeight;
+ 
+new ScrollMagic.Scene({
+  offset: wh*3
+})
+.setClassToggle("section#four", "is-active")
+.addTo(ctrl);
