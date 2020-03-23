7d38011b78c215aa1e23c15eaac406d00f77ac87
Bob <zncodebox@gmail.com>
2020-03-22 20:17:17 -0400

    s

--- js/test.js
+++ js/test.js
@@ -2,22 +2,8 @@
 window.sr = ScrollReveal({ reset: true });
 
 // Custom Settings
-sr.reveal('.foo-1', { duration: 200 });
 
 sr.reveal('.foo-2', { 
   origin: 'right', 
   duration: 2000 
-});
-
-sr.reveal('.foo-3', { 
-  rotate: { x: 100, y: 100, z: 0 },
-  duration: 1000
-});
-
-sr.reveal('.foo-4', { 
-  viewFactor: 0.5
-});
-
-sr.reveal('.foo-5', { 
-  duration: 200 
 });
