d901034497edb0c858ba5fc43c0232d80f19206d
Bob <zncodebox@gmail.com>
2020-03-22 22:37:50 -0400

    big_change

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var http = require('http').Server(app);
 app.use('/html', express.static(path.join(__dirname, 'html')));
 app.use('/js', express.static(path.join(__dirname, 'js')));
 app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(express.json())
 
 app.get('/', function(req,res) {
