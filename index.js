04ab7a6c9dd0937f4c729e4568b27580e790da12
Bob <zncodebox@gmail.com>
2020-03-25 18:07:16 -0400

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var http = require('http').Server(app);
+var nodemailer = require('nodemailer');
 
 // -------------- serve static folders -------------- //
 
@@ -19,6 +20,40 @@ app.get('/', function(req,res) {
 app.get('/test', function(req,res) {
     res.sendFile(__dirname + "/html/test.html");
 })
+
+app.get('/send_email' , function(req, res){
+    
+    console.log("SERVER CALLED")
+    const name = req.query.name
+    const email = req.query.email
+    const message = req.query.message 
+    const no_extension = req.query.name
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'portfolioemail5783@gmail.com',
+            pass: 'nodemailer'
+        }
+    });
+
+    var mailOptions = {
+        from: 'portfolioemail5783@gmail.com',
+        to: 'zncodebox@gmail.com',
+        subject: 'Portfolio Website Message' + 'from' + name + 'at' + email,
+        text: message
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            res.send("1")
+        } else {
+            res.send("0")
+        }
+    });
+
+})
+
 
 
 
--- js/home.js
+++ js/home.js
@@ -8,6 +8,32 @@ sr.reveal('.box_1', {
   duration: 2250 
 });
 
-function sendEmail(){
-  
+function sendEmail()
+{
+  console.log("HELLO")
+  form_data = document.getElementById('message_form');
+  myForm = new FormData(form_data);
+  console.log("AJAX SENT")
+  $.ajax({
+          url: "send_email",
+          type: "get",                         
+          enctype: 'multipart/form-data',
+          processData: false, 
+          contentType: false,            
+          data: myForm,
+          success: function(response) {
+              if(response == "1")
+              {
+                  alert("There has been an error sending your message.  Please check that all entered information is correct!")
+              }
+              else
+              {
+                  alert("Message successfully sent!  I will get back to you as soon as possible.")
+              }
+          },
+          error: function (stat, err) {
+            alert("There has been an error sending your message.  Please check that all entered information is correct!")
+          }       
+  });
+
 }
